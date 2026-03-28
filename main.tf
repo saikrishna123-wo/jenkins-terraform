ea2ae3a62dc0a246f1f26dc2528e550c0f64aa40
saikrishna123-wo <pavankumargottipati1994@gmail.com>
2026-03-28 04:32:33 +0000

    corrected code

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "main.vpc" {
+resource "aws_vpc" "main_vpc" {
     cidr_block = "90.0.0.0/16"
     tags = {
       Name = "pavan-vpc" 
